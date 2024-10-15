b9820824d61f6656d1f4a5c851cce7331c583bfd
dimensions <127557181+Dhiaturki@users.noreply.github.com>
2024-10-15 14:38:49 +0100

    Added table

--- main.py
+++ main.py
@@ -3,11 +3,3 @@ import random
 def create_table(size):
     return [random.randint(0, 100) for _ in range(size)]
 
-def insert_into_sorted_table(table, value):
-    # Find the correct position to insert
-    for i in range(len(table)):
-        if value < table[i]:
-            table.insert(i, value)
-            break
-    else:
-        table.append(value)  # If value is greater than all elements
