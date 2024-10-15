8c8faf5d01df3d75e240b2eb025339843cd77d36
abdelwaheb11 <yazidiabdelwaheb@gmail.com>
2024-10-15 15:39:39 +0100

    bubble sorte

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import random
-
-def create_table(size):
-    return [random.randint(0, 99) for _ in range(size)]
-
-
-def insert_into_sorted_table(table, value):
-    for i in range(len(table)):
-        if value < table[i]:
-            table.insert(i, value)
-            break
-    else:
-        table.append(value)
-
+def bubble_sort(table):
+    n = len(table)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if table[j] > table[j+1]:
+                # Swap the elements
+                table[j], table[j+1] = table[j+1], table[j]
