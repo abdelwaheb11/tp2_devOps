3c302b819fca2461d44899a7038fb53210e106f8
dimensions <127557181+Dhiaturki@users.noreply.github.com>
2024-10-15 14:37:57 +0100

    Added table

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import random
+
+def create_table(size):
+    return [random.randint(0, 100) for _ in range(size)]
+
 def insert_into_sorted_table(table, value):
     # Find the correct position to insert
     for i in range(len(table)):
