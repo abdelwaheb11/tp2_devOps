2c6468f09fb14e70cfe05e60ec1d1b894435cb1e
dimensions <127557181+Dhiaturki@users.noreply.github.com>
2024-10-15 14:31:50 +0100

    Deleted insert.py

--- insert.py
+++
@@ -1,8 +0,0 @@
-def insert_into_sorted_table(table, value):
-    # Find the correct position to insert
-    for i in range(len(table)):
-        if value < table[i]:
-            table.insert(i, value)
-            break
-    else:
-        table.append(value)  # If value is greater than all elements
